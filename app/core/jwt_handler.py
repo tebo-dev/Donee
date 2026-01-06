03c439ce2a4706c3efc7e13f7112771b1a22ace4
Esteban Jumbo <esteban.jumbo.dev@gmail.com>
2026-01-05 22:14:44 -0500

    Set up the creation and verification of JWT tokens.

---
+++ app/core/jwt_handler.py
@@ -0,0 +1,47 @@
+"""Import necessary libraries for jwt tokens creation and handling."""
+
+from datetime import datetime, timedelta, timezone
+from typing import Any
+from jose import jwt
+from app.core.config import settings
+
+
+def create_access_token(data: dict[str, Any],
+                        expires_delta: timedelta | None = None) -> str:
+    """Create a signed JWT access token."""
+
+    to_encode = data.copy()
+
+    now = datetime.now(timezone.utc)
+
+    if expires_delta is None:
+        expires_delta = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    expire = now + expires_delta
+
+    to_encode.update(
+        {
+            "exp": expire,
+            "iat": now,
+        }
+    )
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM,
+    )
+
+    return encoded_jwt
+
+
+def decode_access_token(token: str) -> dict[str, Any]:
+    """Decode and validate a JWT access token."""
+
+    payload = jwt.decode(
+        token,
+        settings.JWT_SECRET_KEY,
+        algorithms=[settings.JWT_ALGORITHM],
+    )
+
+    return payload
