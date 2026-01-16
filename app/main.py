9aeba1024dabadb1b6b6a9c1c41cf66e9b302146
Esteban Jumbo <esteban.jumbo.dev@gmail.com>
2026-01-15 20:41:27 -0500

    Add error box to HTML pages and enable CORS middleware.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 """Import libraries for router implementation."""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes.auth_routes import router as auth_routher
 from app.api.routes.password_reset_routes import router as password_reset_router
@@ -8,6 +9,14 @@ from app.api.routes.password_reset_routes import router as password_reset_router
 app = FastAPI(
     title="Donee API",
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_routher, prefix="/auth", tags=["auth"])
