46e16c258f4947fb56458a897941e0b12dc08f54
Esteban Jumbo <esteban.jumbo.dev@gmail.com>
2026-03-19 23:53:48 -0400

    Add workspace schemas.

---
+++ app/schemas/workspace/workspace.py
@@ -0,0 +1,45 @@
+"""Import the necessary libraries for workspaces implementation."""
+
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, Field
+
+# Requests
+
+
+class WorkspaceCreation(BaseModel):
+    """Schema for workspace creation."""
+
+    model_config = ConfigDict(str_strip_whitespace=True, extra="forbid")
+
+    name: str = Field(min_length=2, max_length=50)
+
+
+class WorkspaceUpdate(BaseModel):
+    """Schema for workspace update."""
+
+    model_config = ConfigDict(str_strip_whitespace=True, extra="forbid")
+
+    name: str = Field(min_length=2, max_length=50)
+
+
+# Responses
+
+
+class WorkspaceOut(BaseModel):
+    """Schema for returning the workspace to the frontend."""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID
+    name: str
+    created_at: datetime
+
+
+class WorkspaceListOut(BaseModel):
+    """Schema for returning a list of workspaces to the frontend."""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    workspaces: list[WorkspaceOut]
